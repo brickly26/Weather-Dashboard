eb130bf2833f4a57278b00855e31a38609fdc85c
Burak Aksu <sabburak26@gmail.com>
2021-08-14 21:00:26 -0400

    hused async await for geocode and onecall

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,14 +10,13 @@ var UVTd = $(".UV")
 var tempWeekEl = $("#weekTemp");
 var prevCity = $("prevCity");
 var asideEl = $(".aside");
-var btnListEl = $(".btnList");
+var btnContainerEl = $(".btnList");
 var cityEl = $(".cityDate");
 
 console.log(asideEl.children().length);
 // DATA
 
-var lata;
-var long;
+
 var historyArr = [];
 
 // FUNCTIONS
@@ -42,6 +41,7 @@ $(function () {
 function onSubmit(event) {
   event.preventDefault();
   var city = inputEl.val();
+  inputEl.val("");
   saveSearch(city);
   renderHistory();
   renderTdWeather(city);
@@ -49,61 +49,74 @@ function onSubmit(event) {
 }
 
 function saveSearch(city) {
-  console.log(historyArr);
-  historyArr.push(city);
-  var strHistory = JSON.stringify(historyArr);
-  localStorage.setItem("History", strHistory);
+  var hisArr = JSON.parse(localStorage.getItem("History"));
+  if (hisArr !== null) {
+    hisArr.unshift(city);
+    var strHistory = JSON.stringify(hisArr);
+    localStorage.setItem("History", strHistory);
+  } else {
+    var newArr = [city];
+    var strHistory = JSON.stringify(newArr);
+    localStorage.setItem("History", strHistory);
+  }
+
 }
 
 function renderHistory() {
   var tempHistory = JSON.parse(localStorage.getItem("History"));
-  btnListEl.remove();
-  btnListEl = $("<div>");
+  btnContainerEl.empty();
   for (var i = 0; i < tempHistory.length; i++) {
     var historyBtn = $("<button>");
-    historyBtn.attr("type", "button");
+    historyBtn.attr("class", "button");
     historyBtn.text(tempHistory[i]);
     historyBtn.attr("data-city", tempHistory[i])
-    btnListEl.append(historyBtn);
+    btnContainerEl.append(historyBtn);
   }
-  asideEl.append(btnListEl);
 }
 
 function showPrevCity() {
-  prevCity.text(localStorage.getItem("City"))
+  return prevCity.text(localStorage.getItem("City"))
 }
 
 function renderTdWeather(city) {
+  GeoCoder(city);
+}
+
+async function GeoCoder(city) {
   var geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=d82badb906f2ae8891cd46df1588137f`;
-  fetch(geoURL)
+  await fetch(geoURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      lata = data[0].lat;
-      long = data[0].lon;
+      var lata = data[0].lat;
+      var long = data[0].lon;
+      OneCall(city, lata, long);
     });
+}
+
+async function OneCall(city, lata,long) {
   var todayURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lata}&lon=${long}&units=imperial&appid=d82badb906f2ae8891cd46df1588137f`;
-  fetch(todayURL)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var today = moment();
-      cityEl.text(`${city}: ${today.format("MMM Do, YYYY")}`);
-      tempTd.text(`Temp: ${(data.current.temp).toFixed(2)}°F`);
-      windTd.text(`Wind: ${data.current.wind_speed} MPH`);
-      humidityTd.text(`Humidity: ${data.current.humidity}%`);
-      var UVIndex = data.current.uvi;
-      UVTd.text(`UV Index: ${UVIndex}`);
-      if (UVIndex <= 2) {
-        UVTd.css("background-color", "green");
-      } else if (UVIndex <= 5) {
-        UVTd.css("background-color", "orange");
-      } else {
-        UVTd.css("background-color", "red");
-      }
-    });
+  await fetch(todayURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          var today = moment();
+          cityEl.text(`${city}: ${today.format("MMM Do, YYYY")}`);
+          tempTd.text(`Temp: ${(data.current.temp).toFixed(2)}°F`);
+          windTd.text(`Wind: ${data.current.wind_speed} MPH`);
+          humidityTd.text(`Humidity: ${data.current.humidity}%`);
+          var UVIndex = data.current.uvi;
+          UVTd.text(`UV Index: ${UVIndex}`);
+          if (UVIndex <= 2) {
+            UVTd.css("background-color", "green");
+          } else if (UVIndex <= 5) {
+            UVTd.css("background-color", "orange");
+          } else {
+            UVTd.css("background-color", "red");
+          }
+        });
 }
 
 function render5DayWeather(city) {
@@ -117,8 +130,8 @@ function render5DayWeather(city) {
       var days = tempWeekEl.children().children();
       
       for (var i = 0; i < days.length; i++) {
-        days.eq(i).children().eq(0).text(`Date: ${today.add(i+1, "days").format("MMM Do, YYYY")}`);
-        days.eq(i).children().eq(1).attr("src", `http://openweathermap.org/img/wn/${data.list[i].weather.icon}@2x.png`)
+        days.eq(i).children().eq(0).text(`Date: ${today.add(1, "days").format("MMM Do, YYYY")}`);
+        days.eq(i).children().eq(1).attr("src", `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`)
         days.eq(i).children().eq(2).text(`Temp: ${data.list[i].main.temp}°F`);
         days.eq(i).children().eq(3).text(`Wind: ${data.list[i].wind.speed} MPH`);
         days.eq(i).children().eq(4).text(`Humidity: ${data.list[i].main.humidity}%`);
@@ -130,16 +143,29 @@ function render5DayWeather(city) {
 // USER INTERACTIONS
 
 // For some reason when the form is submitted it refreshes the page even though i have event.preventDefault();
-formEl.on("submit", onSubmit)
+formEl.on("submit", onSubmit);
 
-historyBtn.click(function(event) {
-  console.log("hello");
+btnContainerEl.on("click", ".button", function(event) {
+  const hisCity = $(event.target).attr("data-city")
+  renderTdWeather(hisCity);
+  render5DayWeather(hisCity);
 });
 
 // INITIALIZATION
 
-// Use this block of code since event.preventDefault(); isnt working
-// renderTdWeather("Austin")
-// render5DayWeather("Austin")
-// showPrevCity();
+function onPageRefresh() {
+  var previousSearches = JSON.parse(localStorage.getItem("History"));
+  if (previousSearches !== null){
+    var previousSearch = previousSearches[0];
+    renderTdWeather(previousSearch);
+    render5DayWeather(previousSearch);
+    renderHistory();
+  } else {
+    renderTdWeather("New York");
+    render5DayWeather("New York");
+    renderHistory();
+  }
+  return;
+}
 
+onPageRefresh();
